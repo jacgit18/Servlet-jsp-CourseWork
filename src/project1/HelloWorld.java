d9a1e2f039ba513a2735db863437125409f85051
jac <joshuacarpentier21@gmail.com>
2020-09-03 20:28:13 -0400

    added classes

---
+++ src/project1/HelloWorld.java
@@ -0,0 +1,29 @@
+package project1;
+
+import java.io.*;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// make sure use only lower case for address
+
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, 
+						HttpServletResponse response)
+		throws ServletException, IOException{
+			
+		
+		PrintWriter out = response.getWriter();
+		out.println("<B> Hello World </B><BR>");
+		out.println("<H1>Hello World</H1>");
+
+	}	
+}
