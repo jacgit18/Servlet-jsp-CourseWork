a7c4c091bbceea63510005746907fab95a5a33cd
jac <joshuacarpentier21@gmail.com>
2020-09-03 22:27:11 -0400

    uncommented import

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -5,14 +5,16 @@ import java.io.*;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // make sure use only lower case for address
 
-//@WebServlet("/hello") // modern way to add path to url using xml is older way but is still used at companies
+@WebServlet("/hello")
+// modern way to add path to url using xml is older way but is still used at companies
+// also xml and java modern way can be enabled at same time  and used.
 public class HelloWorld extends HttpServlet {
 
 	@Override
